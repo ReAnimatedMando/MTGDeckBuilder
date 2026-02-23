ceba3a5f8e100327ef82c92e496624736b55cea4
Aaron Tanguma <the_guma@yahoo.com>
2026-02-22 18:21:33 -0700

    Initial commit: ASP.NET Core MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MTGDeckBuilder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
