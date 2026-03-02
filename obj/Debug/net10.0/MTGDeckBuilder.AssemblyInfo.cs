086ee107cc7570c190d58133e2aabef7ae1825b3
Aaron Tanguma <the_guma@yahoo.com>
2026-03-02 14:00:00 -0700

    up to date

--- obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
+++ obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a08d9c664ecc7d04bb265cb8f04e9ce03aa3c4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bebfd41281d4714da8c5cbee9f40d8a5b303019")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
