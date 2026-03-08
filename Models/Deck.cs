0d71e262dcbaf8140a65fd3e34093fd8973c3042
Aaron Tanguma <the_guma@yahoo.com>
2026-03-08 12:25:59 -0600

    fixed bug in a poorly named file path. cleaned up Deck Builder assembly

--- Models/Deck.cs
+++ Models/Deck.cs
@@ -9,14 +9,14 @@ public class Deck
     public int Id { get; set; }
 
     [Required]
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     public string? Description { get; set; }
 
     // Optional theme like "Goblins", "Control", etc
     public string? Theme { get; set; }
 
-    public string ColorIdentity { get; set; }
+    public string? ColorIdentity { get; set; }
 
     // Navigation
     public ICollection<DeckCard> DeckCards { get; set; } = new List<DeckCard>();
--- obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
+++ obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca326a6e652422333155aecc9f52cf4f41761ae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693c8d62d1c4847e61065e0aee68db734c9579c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
