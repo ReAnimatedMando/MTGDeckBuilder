9288bbef32bbb1b15f3561ddc42e8d85ba7d8376
Aaron Tanguma <the_guma@yahoo.com>
2026-03-21 12:24:20 -0600

    cleaned and refactored site.css

--- obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
+++ obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fce997f24393c8fc8021038a8d34e7715c81c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc06466bef8effa790db2a82f285bc06090f7291")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
