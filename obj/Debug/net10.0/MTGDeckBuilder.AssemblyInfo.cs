e8cd852fecedbbf1cef85506a344430e598f692f
Aaron Tanguma <the_guma@yahoo.com>
2026-04-11 16:44:49 -0600

    added to owned cards

--- obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
+++ obj/Debug/net10.0/MTGDeckBuilder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b714d104f83db5244c26e6dd6c12eb689424a3de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e3c3ffb8544039e0eb47d991082fbd4c5d840d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MTGDeckBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
